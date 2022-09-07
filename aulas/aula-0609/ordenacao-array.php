8ed21f44ed95364a07e4c8af82974c995af4737a
brunosllz <brunoslvrlz13@gmail.com>
2022-09-07 16:36:48 -0300

    palindromos de palavras

--- aulas/aula-0609/ordenacao-array.php
+++ aulas/aula-0609/ordenacao-array.php
@@ -1,5 +1,6 @@
 <?php
 
+//Ordenação crescrente do array
 $numeros = [35, 70, 1, 9, 6, 25, 12];
 $numeros_print = $numeros;
 
@@ -13,4 +14,20 @@ for ($i = 0; $i < count($numeros); $i++) {
   }
 }
 
-echo "Array inicial: " . implode(", ", $numeros_print) . " | Array ordenado: " . implode(", ", $numeros);
+echo "Array inicial: " . implode(", ", $numeros_print) . " | Array ordenado: " . implode(", ", $numeros) . "<br/>";
+
+//Descubrir se a palavra é palindromo
+
+$palavra = "ana";
+$array_palavra = str_split($palavra);
+$palavra_invertida = [];
+
+for ($i = (count($array_palavra) - 1); $i >= 0; $i--) {
+  $palavra_invertida[] = $array_palavra[$i];
+}
+
+if (implode('', $array_palavra) === implode('', $palavra_invertida)) {
+  echo "A palavra $palavra é palindromo";
+} else {
+  echo "A palavra: $palavra  não é palindromo";
+}
